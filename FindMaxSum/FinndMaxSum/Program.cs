cd467fc56ffc2bcf9c3666663f92784b85767b35
DESKTOP-C4PTAPM\Andre Pratama <andre.moerza@gmail.com>
2024-05-06 14:44:40 +0800

    first commit

---
+++ FindMaxSum/FinndMaxSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList arrayList = new ArrayList() { 5, 7, 2, 8, 4, 1, 15 }; // Example array list
+
+        int[] maxSumPair = FindMaxSumPair(arrayList);
+        if (maxSumPair != null)
+        {
+            Console.WriteLine("The pair with the maximum sum is: " + maxSumPair[0] + " and " + maxSumPair[1]);
+            Console.WriteLine("Maximum sum: " + (maxSumPair[0] + maxSumPair[1]));
+        }
+        else
+        {
+            Console.WriteLine("Array list does not contain enough elements to find a pair.");
+        }
+    }
+
+    static int[] FindMaxSumPair(ArrayList arrayList)
+    {
+        if (arrayList.Count < 2)
+            return null; // Not enough elements to form a pair
+
+        int[] maxPair = new int[2];
+        int maxSum = int.MinValue;
+
+        // Iterate through the array list to find the pair with maximum sum
+        for (int i = 0; i < arrayList.Count - 1; i++)
+        {
+            for (int j = i + 1; j < arrayList.Count; j++)
+            {
+                int sum = (int)arrayList[i] + (int)arrayList[j];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxPair[0] = (int)arrayList[i];
+                    maxPair[1] = (int)arrayList[j];
+                }
+            }
+        }
+
+        return maxPair;
+    }
+}
---
+++ FindUniqueNumbers/FindUniqueNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList arrayList = new ArrayList(); // Creating an array list
+        // Get input from the user
+        Console.WriteLine("Enter numbers (enter 'done' when finished):");
+        string input;
+        do
+        {
+            input = Console.ReadLine();
+            if (input.ToLower() != "done")
+            {
+                if (int.TryParse(input, out int num))
+                {
+                    arrayList.Add(num);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number or 'done' to finish.");
+                }
+            }
+        } while (input.ToLower() != "done");
+
+        List<int> uniqueNumbers = FindUniqueNumber(arrayList);
+        if (uniqueNumbers.Count > 0)
+        {
+            Console.WriteLine("The unique numbers in the array list are:");
+            foreach (int num in uniqueNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No unique numbers found in the array list.");
+        }
+    }
+
+    static List<int> FindUniqueNumber(ArrayList arrayList)
+    {
+        // Initialize a dictionary 
+        Dictionary<int, int> numberCount = new Dictionary<int, int>();
+
+        // Count each number in the array list
+        foreach (int num in arrayList)
+        {
+            if (numberCount.ContainsKey(num))
+            {
+                numberCount[num]++;
+            }
+            else
+            {
+                numberCount[num] = 1;
+            }
+        }
+
+        // Find the number that only one 
+        List<int> uniqueNumbers = new List<int>();
+        foreach (var kvp in numberCount)
+        {
+            if (kvp.Value == 1)
+            {
+                uniqueNumbers.Add(kvp.Key);
+            }
+        }
+
+        // Return list of unique numbers
+        return uniqueNumbers;
+    }
+}
+
